c9cbd07c6744814103a3026e46486d1297849576
Leon Pongrac <leon.pongrac@fer.hr>
2023-11-09 14:28:02 +0100

    trebao bi raditi csfr

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,8 @@
 var router = require('express').Router();
+const pgp = require('pg-promise')();
+
+const db = pgp({connectionString: process.env.DATABASE_URL,
+    ssl: {rejectUnauthorized: false}});
 
 const users = [];
 
@@ -7,16 +11,26 @@ router.get('/', function (req, res, next) {
 });
 
 router.post('/register', (req, res) => {
+    console.log('req.body = ' + req.body)
     const { username, password } = req.body;
-    // Validate and handle registration logic (e.g., add user to the database)
-    users.push({ username, password });
+    db.one('INSERT INTO users (username, password) VALUES ($1, $2) RETURNING id', [competitionName, scoringSystem[0], scoringSystem[1], scoringSystem[2], userProfile.email])
+    .then(data => {
+        console.log(data.id);
+        user_id = data.id;
+        users.push({user_id, username, password });
+    })
+    .catch(error => {
+      console.error('Error inserting data into the users table:', error);
+    });
+    
     res.redirect('/SQL');
 });
 
-router.post('/login', (req, res) => {
+router.post('/login', async function (req, res, next) {
     const { username, password } = req.body;
-    // Validate and handle login logic (check if the user exists in the database)
-    const user = users.find(u => u.username === username && u.password === password);
+    const users_data = await db.any('SELECT * FROM users');
+    console.log(users_data);
+    const user = users_data.find(u => u.username === username && u.password === password);
     if (user) {
         res.send('Login successful');
     } else {
@@ -34,6 +48,69 @@ router.get('/CSFR', function (req, res, next) {
 
 router.get('/noCSFR', function (req, res, next) {
     res.render('noCSFR');
+});
+
+router.get('/change-password/:username/:password', (req, res) => {
+    const username = req.params.username;
+    const newPassword = req.params.password;
+
+    const user = users.find(u => u.username === username);
+    const user_id = user.id;
+
+    db.one('UPDATE users SET password = $1 WHERE id = $2', [newPassword, user_id])
+    .then(data => {
+        console.log(data.id);
+    })
+    .catch(error => {
+      console.error('Error inserting data into the users table:', error);
+    });
+
+    // Logic to change the password for the given username
+    // Replace this with your actual password change logic
+
+    // Assuming you have a function to change the password in your database
+    // For example:
+    // changePassword(username, newPassword);
+
+    // Send a response back
+    res.send(`Password changed successfully for user: ${username}`);
+});
+
+const verifySecretToken = (req, res, next) => {
+    const secretToken = req.params.secret;
+
+    // Verify the secret token (replace 'your_secret_token' with your actual secret token)
+    if (secretToken === 'your_secret_token') {
+        next(); // Continue processing the request if the secret token is valid
+    } else {
+        res.status(401).send('Unauthorized'); // Send 401 Unauthorized if the secret token is invalid
+    }
+};
+
+router.post('/change-password-secure/:username/:password/:secret', verifySecretToken, (req, res) => {
+    const username = req.params.username;
+    const newPassword = req.params.password;
+
+    const user = users.find(u => u.username === username);
+    const user_id = user.id;
+
+    db.one('UPDATE users SET password = $1 WHERE id = $2', [newPassword, user_id])
+    .then(data => {
+        console.log(data.id);
+    })
+    .catch(error => {
+      console.error('Error inserting data into the users table:', error);
+    });
+
+    // Logic to change the password for the given username
+    // Replace this with your actual password change logic
+
+    // Assuming you have a function to change the password in your database
+    // For example:
+    // changePassword(username, newPassword);
+
+    // Send a response back
+    res.send(`Password changed successfully for user: ${username}`);
 });
 
 module.exports = router;
